251dd1a9ed9a028977ffb1f3857fb35ea70cdc0c
DESKTOP-EG8UGR5\LAPTOP ASUS <juricaspan@gmail.com>
2023-06-22 14:59:12 -0500

    primer avzance de la api instalcion de codeigniter 4

---
+++ app/Controllers/Api/ProjectsController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Controllers\Api;
+
+use CodeIgniter\RESTful\ResourceController;
+
+class ProjectsController extends ResourceController
+{
+    
+    // Add projects into database, user
+    public function addProject()
+    {
+        //
+    }
+
+    // List projects that belongs to user, througt toke
+    public function listProjects()
+    {
+        //
+    }
+
+    // Delete projects that belongs to user, througt toke
+    public function deleteProject($project_id)
+    {
+        //
+    }
+}
